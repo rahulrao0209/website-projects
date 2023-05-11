9f59b051b423f674e5d505b42bff665920c8de8a
Rahul Rao <63695122+rahulrao0209@users.noreply.github.com>
2023-05-11 22:56:32 +0530

    Crowdfunding | Setting up the initial layouts, components, images and tailwind configs

---
+++ crowdfunding-page/src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Navbar } from "./navbar/Navbar.astro";
+export { default as Header } from "./header/header.astro";
+export { default as Main } from "./main/Main.astro";
--- crowdfunding-page/tailwind.config.cjs
+++ crowdfunding-page/tailwind.config.cjs
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      cyan: {
+        moderate: "hsl(176, 50%, 47%)",
+        dark: "hsl(176, 72%, 28%)",
+      },
+      black: "hsl(0, 0%, 0%)",
+      white: "hsl(100, 100%, 100%)",
+      darkGray: "hsl(0, 0%, 48%)",
+    },
+    fontFamily: {
+      sans: ["Commissioner", "sans-serif"],
+    },
+    extend: {
+      backgroundImage: {
+        "hero-desktop": "url('images/image-hero-desktop.jpg')",
+        "hero-mobile": "url('/images/image-hero-mobile.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
